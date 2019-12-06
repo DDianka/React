ff93dd26e8dd2372f873fee579abab2d09a98539
DDianka <ddianka.1917@gmail.com>
2019-12-06 21:47:46 +0300

    added class instead of function

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {counter: 0, test: "ok"}
+    }
+    makeClick = () => {
+        this.setState( (prevState)  => {return { counter: ++prevState.counter } })
+    }
+    render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         You clicked  {this.state.counter  } times {this.state.test}
         </p>
+        <button onClick={ () => {this.makeClick(this.state.counter)}}> click it </button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +29,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let a = {a:"c", b: "d"};
+ReactDOM.render(<App table= { a } friday={"saturday"}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
