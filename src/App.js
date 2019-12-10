12c9651ccc10ceaf7c15b1ae49653a06616207fb
Diana <ddianka.1917@gmail.com>
2019-12-10 17:25:34 +0300

    almost working react-app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,94 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {counter: 0, test: "ok"}
+        this.state = {
+          userSurname: "",
+          userName: "",
+          userPatronymic: "",
+          users: []
+        }
+    };
+    onSurnameChange = (event) => {
+      this.setState({
+        userSurname: event.target.value
+      });
+    };
+    onNameChange = (event) => {
+      this.setState({
+        userName: event.target.value
+      });
+    };
+    onPatronymicChange = (event) => {
+      this.setState({
+        userPatronymic: event.target.value
+      });
+    };
+     isValid = (userSurname, userName, userPatronymic) => {
+      return userSurname && userName && userPatronymic
+    };
+    onSubmit = () => {
+      if (this.isValid(this.state.userSurname, this.state.userName, this.state.userPatronymic)) {
+      this.setState({
+        users: this.state.users.concat({
+          userSurname: this.state.userSurname,
+          userName: this.state.users.userName,
+          userPatronymic: this.state.users.userPatronymic
+        })
+      })
     }
-    makeClick = () => {
-        this.setState( (prevState)  => {return { counter: ++prevState.counter } })
-    }
+    };
+    onRemoveBtn = (indexToRemove) => {
+      this.setState({
+        users: this.state.users.filter(function (item, index) {
+          return indexToRemove !== index
+        })
+      });
+    };
     render() {
     return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         You clicked  {this.state.counter  } times {this.state.test}
-        </p>
-        <button onClick={ () => {this.makeClick(this.state.counter)}}> click it </button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <label htmlFor="inpSurname">Фамилия</label>
+        <input type="text" value={this.state.userSurname} onChange={this.onSurnameChange} id="inpSurname" className="field"/>
+      </div>
+      <div>
+        <label htmlFor="inpName">Имя</label>
+        <input type="text" value={this.state.userName} onChange={this.onNameChange} className="field"></input>
+      </div>
+      <div>
+        <label htmlFor="inpPatronymic">Отчество</label>
+        <input type="text" value={this.state.userPatronymic} onChange={this.onPatronymicChange} className="field"></input>
+      </div>
+      <button onClick={this.onSubmit} id="btn"> SUBMIT </button>
+      <table>
+        <thead>
+          <tr>
+            <th>Фамилия</th>
+           <th>Имя</th>
+            <th>Отчество</th>
+            <th>Удалить</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            this.state.users.map((user, index) => {
+          return <tr>
+            <td>{user.userSurname}</td>
+            <td>{user.userName}</td>
+            <td>{user.userPatronymic}</td>
+            <td onCLick = {() => this.onRemoveBtn(index)} className="remover">[x]</td>
+          </tr>
+            })
+          }
+        </tbody>
+      </table>
     </div>
-    );
-    }
-}
+    )
+    };
+};
 
 export default App;
